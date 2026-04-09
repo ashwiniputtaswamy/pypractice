2969d27fc23de238e354b09f1d7d311e9a20733a
Ashwini P <128336587+ashwiniputtaswamy@users.noreply.github.com>
2026-04-09 15:25:52 +0530

    Add functions to find largest and smallest numbers
    Implemented multiple functions to find the largest and smallest of three numbers using both custom logic and built-in functions.

---
+++ large_no.py
@@ -0,0 +1,28 @@
+# Finding largest of three numbers using python functions
+def large_no(a,b,c):
+    if a >= b and a >= c:
+        return a
+    elif b >= c and b >= a:
+        return b
+    else:
+        return c
+
+result=large_no(1,30,30)
+print("The largest number is", result)
+
+# Finding largest of 3 numbers using python function and built in max function
+
+def large_no(a,b,c):
+    return max(a,b,c)
+    
+result = large_no(1,5,30)
+print("the largest number",result)
+
+# finding both smallest and largest numbers using python functions and min and max built in functions
+
+def large_no(a,b,c):
+    return max(a,b,c), min(a,b,c)
+
+x,y,z = large_no(1,5,30)
+print(f'Largest = {x}, Smallest = {y}')
+
