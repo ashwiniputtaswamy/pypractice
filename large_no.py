961b0a867c66abf18be8f1dc6d2ea819a9c8409a
Ashwini P <128336587+ashwiniputtaswamy@users.noreply.github.com>
2026-04-10 10:30:07 +0530

    Enhance large_no function to include middle number
    Refactored large_no function to return largest, smallest, and middle values.

--- large_no.py
+++ large_no.py
@@ -11,7 +11,6 @@ result=large_no(1,30,30)
 print("The largest number is", result)
 
 # Finding largest of 3 numbers using python function and built in max function
-
 def large_no(a,b,c):
     return max(a,b,c)
     
@@ -19,10 +18,18 @@ result = large_no(1,5,30)
 print("the largest number",result)
 
 # finding both smallest and largest numbers using python functions and min and max built in functions
-
 def large_no(a,b,c):
     return max(a,b,c), min(a,b,c)
 
 x,y,z = large_no(1,5,30)
 print(f'Largest = {x}, Smallest = {y}')
 
+# finding smallest, largest and the middle number
+def large_no(a,b,c):
+    largest=max(a,b,c)
+    smallest=min(a,b,c)
+    middle=(a+b+c)-(smallest+largest)
+    return largest, smallest, middle
+
+x,y,z=large_no(345,543,30)
+print(f'Largest = {x}, Smallest = {y}, middle = {z}')
