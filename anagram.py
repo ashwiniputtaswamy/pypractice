0195e56ea3202574610334e661d67c349156c5cb
Ashwini P <128336587+ashwiniputtaswamy@users.noreply.github.com>
2026-04-17 11:02:33 +0530

    Implement anagram checker in anagram.py
    Added a Python program to check if two strings are anagrams using the sorted function.

---
+++ anagram.py
@@ -0,0 +1,21 @@
+🧠 What is an Anagram?
+
+Two strings are anagrams if:
+
+They contain the same characters
+In different order
+
+👉 Example:
+
+"listen" and "silent" ✅
+"hello" and "world" ❌
+-------------------------------------------------------------------------------------------------
+# python program to find whether two strings are anagram or not using sorted function
+def is_anagram(s1,s2):
+    return sorted(s1) == sorted(s2)
+print(is_anagram("silent","listen"))
+print(is_anagram("man","nan"))
+
+Output:
+True
+False
