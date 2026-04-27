cdafc7c02728f3f73b31840615ff402374a3d8f4
Ashwini P <128336587+ashwiniputtaswamy@users.noreply.github.com>
2026-04-27 18:44:42 +0530

    square even number-question2

--- Python_scenario_based_interview_questions.py
+++ Python_scenario_based_interview_questions.py
@@ -1,4 +1,6 @@
+----------------------------------------------------------------------------------------------------------------------------------------------------
 # Python scenario based interview questions
+----------------------------------------------------------------------------------------------------------------------------------------------------
 # Question 1: Input String: text = "Analytics2026". Extract only the digits from the string (remove letters and other characters)
 # Expected Output: 2026
 
@@ -8,3 +10,14 @@ digits_only = ''.join([char for char in text if char.isdigit()])
 print(digits_only)
 
 # Output: 2026
+-----------------------------------------------------------------------------------------------------------------------------------------------------
+# Question 2: Input list: numbers = [1,5,2,8,3,7]. Use list comprehension to create a new list with squares of even numbers only.
+# Expected Output: [4, 64]
+
+numbers = [1,5,2,8,3,7]
+
+sq_even_no = [x ** 2 for x in numbers if x % 2 == 0]
+print(sq_even_no)
+
+# Output: [4, 64]
+-----------------------------------------------------------------------------------------------------------------------------------------------------
